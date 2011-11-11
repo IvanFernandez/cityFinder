f97b4a9a57ad14105dd021996191d4b98a5d4911
Ivan Fernandez <ivan.fernandez.perea@gmail.com>
2011-11-11 02:34:16 +0100

    added a info page for madrid built with zazar's jquery plugins

--- grails-app/controllers/cityfinder/CityController.groovy
+++ grails-app/controllers/cityfinder/CityController.groovy
@@ -4,100 +4,131 @@ import org.springframework.dao.DataIntegrityViolationException
 
 class CityController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
+	def info() {
+		def cityInstance = City.get(params.id)
+		render(view: "info", model: [cityInstance: cityInstance])
+		return
+	}
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def list() {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [cityInstanceList: City.list(params), cityInstanceTotal: City.count()]
-    }
+	def list() {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[cityInstanceList: City.list(params), cityInstanceTotal: City.count()]
+	}
 
-    def create() {
-        [cityInstance: new City(params)]
-    }
+	def create() {
+		[cityInstance: new City(params)]
+	}
 
-    def save() {
-        def cityInstance = new City(params)
-        if (!cityInstance.save(flush: true)) {
-            render(view: "create", model: [cityInstance: cityInstance])
-            return
-        }
+	def save() {
+		def cityInstance = new City(params)
+		if (!cityInstance.save(flush: true)) {
+			render(view: "create", model: [cityInstance: cityInstance])
+			return
+		}
 
-		flash.message = message(code: 'default.created.message', args: [message(code: 'city.label', default: 'City'), cityInstance.id])
-        redirect(action: "show", id: cityInstance.id)
-    }
+		flash.message = message(code: 'default.created.message', args: [
+			message(code: 'city.label', default: 'City'),
+			cityInstance.id
+		])
+		redirect(action: "show", id: cityInstance.id)
+	}
 
-    def show() {
-        def cityInstance = City.get(params.id)
-        if (!cityInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def show() {
+		def cityInstance = City.get(params.id)
+		if (!cityInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        [cityInstance: cityInstance]
-    }
+		[cityInstance: cityInstance]
+	}
 
-    def edit() {
-        def cityInstance = City.get(params.id)
-        if (!cityInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def edit() {
+		def cityInstance = City.get(params.id)
+		if (!cityInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        [cityInstance: cityInstance]
-    }
+		[cityInstance: cityInstance]
+	}
 
-    def update() {
-        def cityInstance = City.get(params.id)
-        if (!cityInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def update() {
+		def cityInstance = City.get(params.id)
+		if (!cityInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        if (params.version) {
-            def version = params.version.toLong()
-            if (cityInstance.version > version) {
-                cityInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'city.label', default: 'City')] as Object[],
-                          "Another user has updated this City while you were editing")
-                render(view: "edit", model: [cityInstance: cityInstance])
-                return
-            }
-        }
+		if (params.version) {
+			def version = params.version.toLong()
+			if (cityInstance.version > version) {
+				cityInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[
+							message(code: 'city.label', default: 'City')]
+						as Object[],
+						"Another user has updated this City while you were editing")
+				render(view: "edit", model: [cityInstance: cityInstance])
+				return
+			}
+		}
 
-        cityInstance.properties = params
+		cityInstance.properties = params
 
-        if (!cityInstance.save(flush: true)) {
-            render(view: "edit", model: [cityInstance: cityInstance])
-            return
-        }
+		if (!cityInstance.save(flush: true)) {
+			render(view: "edit", model: [cityInstance: cityInstance])
+			return
+		}
 
-		flash.message = message(code: 'default.updated.message', args: [message(code: 'city.label', default: 'City'), cityInstance.id])
-        redirect(action: "show", id: cityInstance.id)
-    }
+		flash.message = message(code: 'default.updated.message', args: [
+			message(code: 'city.label', default: 'City'),
+			cityInstance.id
+		])
+		redirect(action: "show", id: cityInstance.id)
+	}
 
-    def delete() {
-        def cityInstance = City.get(params.id)
-        if (!cityInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def delete() {
+		def cityInstance = City.get(params.id)
+		if (!cityInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        try {
-            cityInstance.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'city.label', default: 'City'), params.id])
-            redirect(action: "show", id: params.id)
-        }
-    }
+		try {
+			cityInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "list")
+		}
+		catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [
+				message(code: 'city.label', default: 'City'),
+				params.id
+			])
+			redirect(action: "show", id: params.id)
+		}
+	}
 }
--- grails-app/domain/cityfinder/City.groovy
+++ grails-app/domain/cityfinder/City.groovy
@@ -3,6 +3,7 @@ package cityfinder
 class City {
 	String name
 	String country
+
     static constraints = {
     	name blank : false
     	country blank : false
